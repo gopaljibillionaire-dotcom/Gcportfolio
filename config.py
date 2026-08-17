eac40a5ce26a6ce6d4886f7da900c1aa4f5bd6ea
gopaljibillionaire-dotcom <gopaljibillionaire@gmail.com>
2026-08-17 23:17:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ BIN_CHANNEL_RAW = os.getenv("BIN_CHANNEL", "-1004412044372").strip()
 try:
     BIN_CHANNEL = int(BIN_CHANNEL_RAW)
 except ValueError:
-    BIN_CHANNEL = -1004416159243
+    BIN_CHANNEL = -1004412044372
 
 REQUIRED_CHANNEL_USERNAME = os.getenv("REQUIRED_CHANNEL_USERNAME", "yagamicorporation").replace("@", "").strip()
 
